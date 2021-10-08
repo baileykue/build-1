65b1e261f54d2c938213f978ecdddd5c4eb8ab43
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-10-08 14:05:20 -0700

    the button now generates a new number every time it is clicked, fixed this after the hour was up

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-let randomNum = Math.floor(Math.random() * 100);
 const button = document.getElementById('generate');
 const text = document.getElementById('random-num');
 
 button.addEventListener('click', ()=>{
+    let randomNum = Math.floor(Math.random() * 100);
     text.textContent = randomNum;
+
 });
 
